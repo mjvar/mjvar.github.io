5e9e087edeb6430398d94a3e6dd7df2e49bb5e10
NYUAD <nyuad@a-10-27-24-132.dynapool.vpn.nyu.edu>
2020-12-17 13:29:53 +0400

    Fixing layouts

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
 }
 
 function draw(){
-	step = max(width,height)/5;
+	step = max(width,height)/4;
 	background(250);
 	for(let x = -step; x <= width + step; x += step){
 		for(let y = -step; y <= height + step; y += step){
@@ -38,4 +38,23 @@ function drawCirc(x, y, mouseDist, fillFactor, agitateFactor){
 	ellipse(x, y - agitateFactor/2, step/1.3, step/1.3);
 	fill(255, 251, 25, fillFactor);
 	ellipse(x - agitateFactor/3, y + agitateFactor/3, step/1.3, step/1.3);
+}
+
+let projectPopup = document.querySelector(".project-popup");
+let popCont = document.querySelector(".popup-container");
+
+function toggleProjectPopup(proj){
+	projectPopup.classList.toggle("active");
+
+	if(proj == "sonar"){
+		popCont.innerHTML = `
+				<div class="popup-img">
+					<img src="media/sonar-dj.png">
+				</div>
+				<h1>Sonar DJ</h1>
+				<h5><a href="index.html" class="projlink">link</a> | <a href="index.html" class="codelink">code</a></h5>
+				<p>
+					Sonar DJ is an interactive electronic music installation made with Java, Processing, and Arduino. A sonar sensor constantly scans a mat in front of it. By placing different objects on the mat and moving them around, the user can trigger different sound loops and jam along to them live.
+				</p>`;
+	}
 }
