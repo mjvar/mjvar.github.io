aa17bd25ec46b0451b63b1626cbcc0a915b19547
mjvar <mjv379@nyu.edu>
2025-06-19 22:17:27 -0400

    trying more mobile friendly hydra fallback

--- assets/js/hydra.js
+++ assets/js/hydra.js
@@ -1,13 +1,18 @@
 window.onload = function() {
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    
+    console.log('isMobile:', isMobile);
     const hydra = new Hydra({
-      detectAudio: false,
-      enableStreamCapture: false,
-      width: window.innerWidth,
-      height: window.innerHeight,
+        detectAudio: false,
+        enableStreamCapture: false,
+        width: window.innerWidth,
+        height: window.innerHeight,
     });
 
-    // p5
-    p5 = new P5()
+    p5 = new P5({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
     
 
     p5.hide();
@@ -41,10 +46,6 @@ window.onload = function() {
 
       p5.text(currentPhrase, p5.width/2, p5.height/2);
     }
-
-    p5.mouseClicked = ()=>{
-      phraseIndex++;
-    };
     
     p5.mouseWheel = (event)=>{
       console.log(noiseVal, event.delta);
@@ -62,21 +63,40 @@ window.onload = function() {
     // hydra
     s0.init({src: p5.canvas})
 
-    src(s0).out()
+    if (isMobile) {
+        // Simple version for mobile
+        src(s0).out()
 
-    console.log(noiseVal);
-    osc(1,0.01,1).modulate(noise(()=>noiseVal,1),1).out(o1)
+        osc(10,0.01,1).modulate(noise(100,0.5),1).out(o1)
 
-    src(o2).modulate(
-      src(o1).add(solid(
-        ()=>p5.map(p5.mouseX,0,p5.width,2,-2),
-        ()=>p5.map(p5.mouseY,0,p5.height,2,0.5)),-1),
-      ()=>p5.map((Math.abs(p5.mouseX-p5.width/2)),0,p5.width/2,0.0,.01))
-    .blend(
-      src(o0),()=>p5.map(p5.mouseY,p5.height/4,p5.height,yMax,yMax/4))
-    .out(o2)
+        src(o2).modulate(src(o1).blend(solid(-1,1),2),0.01)
+        .blend(src(o0),1.1)
+        .out(o2)
 
-    src(o0).blend(o2,()=>Math.pow((Math.abs(p5.width/2-p5.mouseX)),1.1)).out(o3)
+        src(o0).blend(o2,100).out(o3)
 
-    render(o3)
+        render(o3)
+    } else {
+      p5.mouseClicked = ()=>{
+        phraseIndex++;
+      };  
+
+      src(s0).out()
+
+      console.log(noiseVal);
+      osc(1,0.01,1).modulate(noise(()=>noiseVal,1),1).out(o1)
+
+      src(o2).modulate(
+        src(o1).add(solid(
+          ()=>p5.map(p5.mouseX,0,p5.width,2,-2),
+          ()=>p5.map(p5.mouseY,0,p5.height,2,0.5)),-1),
+        ()=>p5.map((Math.abs(p5.mouseX-p5.width/2)),0,p5.width/2,0.0,.01))
+      .blend(
+        src(o0),()=>p5.map(p5.mouseY,p5.height/4,p5.height,yMax,yMax/4))
+      .out(o2)
+
+      src(o0).blend(o2,()=>Math.pow((Math.abs(p5.width/2-p5.mouseX)),1.1)).out(o3)
+
+      render(o3)
+    }
   }
